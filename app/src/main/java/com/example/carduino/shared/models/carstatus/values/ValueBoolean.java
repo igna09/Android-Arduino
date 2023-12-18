17464b834be4b9e3681d0616c9c950e58a942db0
ignazio abbate <ignazio.abbate95@gmail.com>
2023-12-18 15:44:03 +0100

    fixed error

--- app/src/main/java/com/example/carduino/shared/models/carstatus/values/ValueBoolean.java
+++ app/src/main/java/com/example/carduino/shared/models/carstatus/values/ValueBoolean.java
@@ -4,6 +4,9 @@ public class ValueBoolean extends Value<Boolean> {
     public ValueBoolean(String id, Boolean value, String unit) {
         super(id, value, unit);
     }
+    public ValueBoolean() {
+        super();
+    }
 
     @Override
     public Boolean parseValueFromString(String value) {
