e13778cfcb7bbf8b30d6496ee91f1bcfb7ad5a7e
ignazio abbate <i.abbate@reactive-almaviva.it>
2024-01-17 11:18:02 +0100

    added distance calculation log

--- app/src/main/java/com/example/carduino/shared/models/carstatus/propertychangelisteners/SpeedCarStatusPropertyChangeListener.java
+++ app/src/main/java/com/example/carduino/shared/models/carstatus/propertychangelisteners/SpeedCarStatusPropertyChangeListener.java
@@ -3,6 +3,7 @@ package com.example.carduino.shared.models.carstatus.propertychangelisteners;
 import com.example.carduino.shared.models.carstatus.values.KmhSpeed;
 import com.example.carduino.shared.models.trip.tripvalue.TripValueEnum;
 import com.example.carduino.shared.singletons.TripSingleton;
+import com.example.carduino.shared.utilities.LoggerUtilities;
 
 import java.util.Date;
 
@@ -21,9 +22,19 @@ public class SpeedCarStatusPropertyChangeListener extends PropertyChangeListener
             }
             if(oldValue.getValue() > 0) { // excluding first speed after start to avoid wrong values when app doesn't remove singleton from memory at car shutdown
                 Float avgSpeed = new Float((oldValue.getValue() + newValue.getValue()) / 2.0);
-                Float deltaT = ((Double) (Math.abs((new Date()).getTime() - TripSingleton.getInstance().getTrip().getTripValues().get(TripValueEnum.DISTANCE).getLastReading().getTime()) / 1000.0)).floatValue();
+                Long nowTime = (new Date()).getTime();
+                Float deltaT = ((Double) (Math.abs(nowTime - TripSingleton.getInstance().getTrip().getTripValues().get(TripValueEnum.DISTANCE).getLastReading().getTime()) / 1000.0)).floatValue();
                 Float distance = (((avgSpeed * deltaT) / 3600) * 1/*distCorrVal*/);
-//            LoggerUtilities.logMessage("SpeedCarStatusPropertyChangeListener", "Distance " + distance);
+                LoggerUtilities.logMessage(
+                        "SpeedCarStatusPropertyChangeListener",
+                        "oldValue " + oldValue.getValue()
+                                + ", newValue " + newValue.getValue()
+                                + ", avgSpeed " + avgSpeed
+                                + ", lastReading " + TripSingleton.getInstance().getTrip().getTripValues().get(TripValueEnum.DISTANCE).getLastReading().getTime()
+                                + ", nowTime " + nowTime
+                                + ", deltaT " + deltaT
+                                + ", distance((avgSpeed * deltaT) / 3600) " + distance
+                        );
                 TripSingleton.getInstance().getTrip().addTripValue(TripValueEnum.DISTANCE, distance);
             }
         }
